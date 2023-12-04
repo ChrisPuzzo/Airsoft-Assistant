5efd6fb55d0f9c09c47e6bc51a908cec9f7f2e30
NickPhenner <nicholas.phenner@gmail.com>
2023-12-04 13:59:47 -0500

    finished game interface - nerf revolution

--- nerfBase/nerfUser/nerfOrNothing/gameInterface.java
+++ nerfBase/nerfUser/nerfOrNothing/gameInterface.java
@@ -1,5 +1,11 @@
 package nerfOrNothing;
 
 public interface gameInterface {
-
+	private int playerNumbers;
+	private String[] playerArray;
+	private void gameRules(); //displays game rules
+	private void addPlayers(); //add users to game
+	public void startGame(); //starts the game
+	private void elimination(); //registers hits
+	private void teamGen(); //generates teams - not priority
 }
